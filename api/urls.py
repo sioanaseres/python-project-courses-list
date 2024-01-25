24034f87104753927855d3d084db3cde63a41368
Ioana Cosoreanu-Seres <ioana.cosoreanuseres@888holdings.com>
2024-01-25 21:36:23 +0200

    first commit

---
+++ api/urls.py
@@ -0,0 +1,13 @@
+from api.models import CategoryResource, CourseResource
+from tastypie.api import Api
+from django.urls import path, include
+
+api = Api(api_name = "v1")
+
+api.register(CourseResource())
+api.register(CategoryResource())
+
+
+urlpatterns = [
+     path('', include(api.urls) ),
+]
---
+++ shop/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name  = "shop"
+
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("<int:course_id>", views.single_course, name="single_course")
+]
